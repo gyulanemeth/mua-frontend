8d12c40808def13111d30e69f104b2aa321da71e
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-08-30 04:01:33 +0200

    set title from config

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
+import { onMounted } from 'vue'
 import NavBar from './components/NavBar.vue'
 import SideBar from './components/SideBar.vue'
 import ErrorMessage from './components/ErrorMessage.vue'
 import { useCurrentUserAndAccountStore } from './stores/index.js'
 
 const store = useCurrentUserAndAccountStore()
+
+onMounted(() => {
+  document.title = window.config.appTitle
+})
+
 </script>
 <template>
   <v-app>
