2eb9f2c0b9adf758dd5516597d018d3187dab4ac
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-08-30 15:12:34 +0200

    Update SideBar.vue

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -14,13 +14,28 @@ function redirect () {
   <v-card class="elevation-4">
 
       <v-navigation-drawer color="grey-lighten-2" rail permanent >
+
         <v-list bg-color="grey-lighten-2" density="compact" nav>
-          <v-list-item active-class="text-white"  prepend-icon="mdi-shield-account-variant-outline"  />
+      <v-list-item class="justify-center align-center" active-class=" elevation-4 text-white bg-white">
+              <v-list-item-icon class="text-black">
+                mdi-shield-account-variant-outline
+              </v-list-item-icon>
+            </v-list-item>
 
-          <v-list-item  active-class="text-white" active prepend-icon="mdi-account-group-outline" />
+            <v-list-item class="justify-center align-center" active active-class=" elevation-4 text-white bg-white">
+                    <v-list-item-icon class="text-black">
+                      mdi-account-group-outline
+                    </v-list-item-icon>
+                  </v-list-item>
 
-          <v-list-item  active-class="text-white" prepend-icon="mdi-email-variant" @click="redirect" />
-        </v-list>
+
+                  <v-list-item class="justify-center align-center" active-class=" elevation-4 text-white bg-white"  @click="redirect" >
+                          <v-list-item-icon class="text-black">
+                          mdi-email-variant
+                          </v-list-item-icon>
+                        </v-list-item>
+
+              </v-list>
 
       </v-navigation-drawer>
 
