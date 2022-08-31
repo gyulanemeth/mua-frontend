f1a66145f7482a93f33c7143d258f0dc36ae3f9f
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-08-31 22:28:50 +0200

    add title and update alert message style

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <script setup>
+import { useRoute } from 'vue-router'
 import { useCurrentUserAndAccountStore } from '../stores/index.js'
 
 const store = useCurrentUserAndAccountStore()
+const route = useRoute()
 
 const menuItems = [{
   title: 'Me',
@@ -21,18 +23,25 @@ const appIcon = window.config.appIcon
 
 <template>
 
-<v-app-bar v-if="store.user" class="elevation-0 pl-0 ml-0">
+<v-app-bar v-if="store.user" height="100" class="elevation-0 pl-0 ml-0">
     <v-app-bar-nav-icon size="60" color="info" :icon="appIcon" />
-    <v-app-bar-title class=" text-h4">{{appName}}</v-app-bar-title>
+    <v-app-bar-title > <h4 class=" text-h4">
+      <span class="mx-1 pt-0 "> {{appName}} </span>
+      <span class="mx-7 pt-0 "> {{ route.name === "me"? "My Profile": route.name === "users"? "Account Users": route.name === "account" ? "Account Settings" : null }}
+      </span>
+      </h4>
+    </v-app-bar-title>
     <v-spacer></v-spacer>
 
     <v-menu location="bottom " origin="end top">
         <template v-slot:activator="{ props }">
-            <v-avatar size="large" color="grey-darken-3">
+          <v-badge color="error" bordered offset-x="10" offset-y="34" icon="mdi-shield-account-variant-outline">
+            <v-avatar size="large" color="error">
                 <v-btn v-bind="props">
                     Pic
                 </v-btn>
             </v-avatar>
+          </v-badge>
         </template>
         <v-list>
             <v-list-item v-for="item in menuItems" :key="item.title" :to="item.path" :value="item.title">
