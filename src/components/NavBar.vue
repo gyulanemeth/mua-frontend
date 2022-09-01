434149ce4111a72e8e9ddd696acc5c0a811fe07c
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-09-01 23:27:19 +0200

    navbar appTitle

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -24,15 +24,17 @@ const appIcon = window.config.appIcon
 <template>
 
 <v-app-bar v-if="store.user" height="100" class="elevation-0 pl-0 ml-0">
-    <v-avatar size="80" >
+    <v-avatar size="60" >
       <v-img :src="appIcon" cover></v-img>
     </v-avatar>
-    <v-app-bar-title > <h4 class=" text-h4">
-      <span class="mx-1 pt-0 "> {{appName}} </span>
-      <span class="mx-7 pt-0 "> {{ route.name === "me"? "My Profile": route.name === "users"? "Account Users": route.name === "account" ? "Account Settings" : null }}
-      </span>
-      </h4>
-    </v-app-bar-title>
+
+   <v-col cols="2">
+    <span class="text-h4 mx-1 pt-0 "> {{appName}} </span>
+  </v-col>
+  <span class="text-h4 ma-0 pt-0"> {{ route.name === "me"? "My Profile": route.name === "users"? "Account Users": route.name === "account" ? "Account Settings" : null }}
+    </span>
+
+
     <v-spacer></v-spacer>
 
     <v-menu location="bottom " origin="end top">
