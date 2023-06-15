4dce3bf3de91a68c3bdaad3be72f9478e790efca
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2023-06-15 12:12:54 +0200

    replace profilePicture with logo

--- src/components/AdminSettings.vue
+++ src/components/AdminSettings.vue
@@ -4,7 +4,7 @@ import DeleteMyAccount from './DeleteMyAccount.vue'
 import { useCurrentUserAndAccountStore } from '../stores/index.js'
 import alerts from '../alerts/alert.js'
 
-const emit = defineEmits(['deleteEventHandler', 'uploadProfilePictureHandler', 'deleteProfilePictureHandler'])
+const emit = defineEmits(['deleteEventHandler'])
 const props = defineProps({
   data: Object
 })
@@ -19,17 +19,17 @@ const settings = ref({})
 const alert = alerts()
 const store = useCurrentUserAndAccountStore()
 await store.readOne()
-const defaultProfilePicture = import.meta.env.BASE_URL + 'placeholder.jpg'
-const profilePicture = ref(store.account.profilePicture)
+const defaultLogo = import.meta.env.BASE_URL + 'placeholder.jpg'
+const logo = ref(store.account.logo)
 
 const submit = async () => {
   btnProcessing.value = true
   let res
-  if (settings.value.profilePicture === null) {
-    res = await store.deleteAccountProfilePicture()
-  } else if (settings.value.profilePicture) {
-    res = await store.uploadAccountProfilePicture(settings.value.profilePicture)
-    profilePicture.value = res.profilePicture
+  if (settings.value.logo === null) {
+    res = await store.deleteAccountLogo()
+  } else if (settings.value.logo) {
+    res = await store.uploadAccountLogo(settings.value.logo)
+    logo.value = res.logo
   }
   if (res && !res.message) {
     alert.message('Updated Successfully')
@@ -37,30 +37,30 @@ const submit = async () => {
   btnProcessing.value = false
 }
 
-const removeProfilePicture = () => {
-  profilePicture.value = import.meta.env.BASE_URL + 'placeholder.jpg'
-  settings.value.profilePicture = null
-  profilePicture.value = null
+const removeLogo = () => {
+    logo.value = import.meta.env.BASE_URL + 'placeholder.jpg'
+  settings.value.logo = null
+  logo.value = null
 }
 
 const handleFileChange = (event) => {
   processing.value = true
   const formData = new FormData()
-  formData.append('profilePicture', event.target.files[0])
-  settings.value.profilePicture = formData
+  formData.append('logo', event.target.files[0])
+  settings.value.logo = formData
   previewImage(event.target.files[0])
   processing.value = false
 }
 
 const openFileInput = () => {
-  const fileInput = document.getElementById('accountSettingProfilePicture')
+  const fileInput = document.getElementById('accountSettingLogo')
   fileInput.click()
 }
 
 const previewImage = (file) => {
   const reader = new FileReader()
   reader.onload = () => {
-    profilePicture.value = reader.result
+    logo.value = reader.result
   }
   reader.readAsDataURL(file)
 }
@@ -88,17 +88,17 @@ const previewImage = (file) => {
             </v-progress-circular>
           </div>
 
-          <v-img v-else :src="profilePicture || defaultProfilePicture" height="150px" cover></v-img>
-          <input ref="fileInput" type="file" id="accountSettingProfilePicture" style="display: none" @change="handleFileChange">
+          <v-img v-else :src="logo || defaultLogo" height="150px" cover></v-img>
+          <input ref="fileInput" type="file" id="accountSettingLogo" style="display: none" @change="handleFileChange">
 
           <v-card-title class="justify-center py-0">
-            <v-btn v-if="profilePicture" @click="removeProfilePicture" color="red" variant="text" icon="mdi-delete-forever-outline"
+            <v-btn v-if="logo" @click="removeLogo" color="red" variant="text" icon="mdi-delete-forever-outline"
               size="small" />
             <v-btn v-else color="grey-lighten-1" @click="openFileInput" variant="text" icon="mdi-image-plus"
               size="small" />
           </v-card-title>
           <v-row class="justify-center py-3 ">
-            <p v-if="profilePicture">{{ $t('adminSettings.picDeleteSubmit') }}</p>
+            <p v-if="logo">{{ $t('adminSettings.picDeleteSubmit') }}</p>
             <p v-else>{{ $t('adminSettings.picSubmit') }}</p>
           </v-row>
         </v-card>
