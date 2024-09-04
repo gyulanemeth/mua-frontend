23fdc76923aa7423b98f2654e6764ca3ee236f86
AhmedQudaih <ahmedku0123@gmail.com>
2024-09-04 14:22:38 +0300

    update

--- src/components/AdminDashboard.vue
+++ src/components/AdminDashboard.vue
@@ -9,109 +9,148 @@ const statsStore = useStatsStore()
 
 const accounts = ref(await statsStore.getAccountsStats({ limit: 'unlimited', sort: { createdAt: 1 } }))
 const users = ref(await statsStore.getUsersStats({ limit: 'unlimited', sort: { createdAt: 1 } }))
-const groupedAccountByDate = {}
-const groupedUsersByDate = {}
+
+const groupedAccountByMonth = {}
+const groupedUsersByMonth = {}
+const groupedDeletedAccountByMonth = {}
+const groupedDeletedUsersByMonth = {}
+
+const getMonthYear = (dateStr) => {
+  const date = new Date(dateStr)
+  return `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}`
+}
 
 accounts.value.items.forEach(item => {
-  if (item.createdAt) {
-    const date = item.createdAt.split('T')[0] // Format: YYYY-MM-DD
-    groupedAccountByDate[date] = (groupedAccountByDate[date] || 0) + 1
+  if (item.createdAt && !item.deleted) {
+    const month = getMonthYear(item.createdAt)
+    groupedAccountByMonth[month] = (groupedAccountByMonth[month] || 0) + 1
+  } else if (item.deleted) {
+    const month = getMonthYear(item.updatedAt)
+    groupedDeletedAccountByMonth[month] = (groupedDeletedAccountByMonth[month] || 0) + 1
   }
 })
+
 users.value.items.forEach(item => {
-  if (item.createdAt) {
-    const date = item.createdAt.split('T')[0] // Format: YYYY-MM-DD
-    groupedUsersByDate[date] = (groupedUsersByDate[date] || 0) + 1
+  if (item.createdAt && !item.deleted) {
+    const month = getMonthYear(item.createdAt)
+    groupedUsersByMonth[month] = (groupedUsersByMonth[month] || 0) + 1
+  } else if (item.deleted) {
+    const month = getMonthYear(item.updatedAt)
+    groupedDeletedUsersByMonth[month] = (groupedDeletedUsersByMonth[month] || 0) + 1
   }
 })
 
-const dates = [
-  ...Object.keys(groupedAccountByDate),
-  ...Object.keys(groupedUsersByDate)
-]
-const minDate = new Date(Math.min(...dates.map(date => new Date(date).getTime())))
-const todayDate = new Date()
+const months = [
+  ...new Set([
+    ...Object.keys(groupedAccountByMonth),
+    ...Object.keys(groupedUsersByMonth),
+    ...Object.keys(groupedDeletedAccountByMonth),
+    ...Object.keys(groupedDeletedUsersByMonth)
+  ])
+].sort()
 
-const createSeries = (data) => {
-  const seriesData = []
-  let currentDate = new Date(minDate)
-  while (currentDate <= todayDate) {
-    const dateStr = currentDate.toISOString().split('T')[0]
-    seriesData.push({
-      x: dateStr,
-      y: data[dateStr] || 0
-    })
-    currentDate = new Date(currentDate)
-    currentDate.setDate(currentDate.getDate() + 1)
-  }
-  return seriesData
+const createMonthlySeries = (data, deleted) => {
+  return months.map(month => ({
+    x: month,
+    y: (deleted ? -data[month] : data[month]) || 0
+  }))
 }
 
-const createAccumulatedSeries = (data) => {
-  const seriesData = []
-  let currentDate = new Date(minDate)
-  let accumulatedCount = 0
-
-  while (currentDate <= todayDate) {
-    const dateStr = currentDate.toISOString().split('T')[0]
-    accumulatedCount += data[dateStr] || 0
-    seriesData.push({
-      x: dateStr,
-      y: accumulatedCount
-    })
-    currentDate = new Date(currentDate)
-    currentDate.setDate(currentDate.getDate() + 1)
-  }
-  return seriesData
+const createCumulativeSeries = (data, deleted) => {
+  let cumulativeTotal = 0
+  return months.map(month => {
+    cumulativeTotal += data[month] || 0
+    return {
+      x: month,
+      y: deleted ? -cumulativeTotal : cumulativeTotal
+    }
+  })
 }
 
-const accumulatedSeriesData = createAccumulatedSeries(groupedAccountByDate)
-const accumulatedSeriesUsersData = createAccumulatedSeries(groupedUsersByDate)
-const seriesData = createSeries(groupedAccountByDate)
-const seriesUsersData = createSeries(groupedUsersByDate)
+const usersSeries = ref([
+  {
+    name: t('mua.adminDashboard.chart.accumulatedUsersLabel'),
+    data: createCumulativeSeries(groupedUsersByMonth),
+    color: '#0859B3'
+  },
+  {
+    name: t('mua.adminDashboard.chart.usersLabel'),
+    data: createMonthlySeries(groupedUsersByMonth),
+    color: '#10B1EF'
+  },
+  {
+    name: t('mua.adminDashboard.chart.deletedAccumulatedUsersLabel'),
+    data: createCumulativeSeries(groupedDeletedUsersByMonth, true),
+    color: '#B71C1C'
+  },
+  {
+    name: t('mua.adminDashboard.chart.deletedUsersLabel'),
+    data: createMonthlySeries(groupedDeletedUsersByMonth, true),
+    color: '#E57373'
+  }
+])
 
-const series1 = [{
-  name: t('mua.adminDashboard.chart.accountsLabel'),
-  data: seriesData
-}, {
-  name: t('mua.adminDashboard.chart.usersLabel'),
-  data: seriesUsersData
-}]
+const accountsSeries = ref([
+  {
+    name: t('mua.adminDashboard.chart.accumulatedAccountsLabel'),
+    data: createCumulativeSeries(groupedAccountByMonth),
+    color: '#0859B3'
+  },
+  {
+    name: t('mua.adminDashboard.chart.accountsLabel'),
+    data: createMonthlySeries(groupedAccountByMonth),
+    color: '#10B1EF'
+  },
+  {
+    name: t('mua.adminDashboard.chart.deletedAccumulatedUsersLabel'),
+    data: createCumulativeSeries(groupedDeletedAccountByMonth, true),
+    color: '#B71C1C'
+  },
+  {
+    name: t('mua.adminDashboard.chart.deletedAccountsLabel'),
+    data: createMonthlySeries(groupedDeletedAccountByMonth, true),
+    color: '#E57373'
+  }
+])
 
-const series2 = [{
-  name: t('mua.adminDashboard.chart.accountsLabel'),
-  data: accumulatedSeriesData
-}, {
-  name:  t('mua.adminDashboard.chart.usersLabel'),
-  data: accumulatedSeriesUsersData
-}]
-
-const interval = 10
-const options = {
-  colors: ['#00BFA5', '#10B1EF'],
+// Chart options
+const options = ref({
   chart: {
-    type: 'line',
+    type: 'bar',
+    stacked: true,
     zoom: {
       enabled: false
     }
   },
   xaxis: {
-    type: 'datetime',
+    type: 'category',
     labels: {
       formatter: function (value) {
-        const date = new Date(value)
-        return `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`
+        if (value) {
+          const [year, month] = value.split('-')
+          return `${year}-${month}`
+        }
       }
-    },
-    tickAmount: Math.ceil((todayDate - minDate) / (interval * 24 * 60 * 60 * 1000)) + 1
+    }
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false
+    }
+  },
+  fill: {
+    opacity: 1
+  },
+  legend: {
+    position: 'top'
   }
-}
+})
 </script>
 
 <template>
   <div class="mx-3">
     <v-layout :class="`d-flex flex-wrap`">
-      <v-card flat :width="$vuetify.display.mdAndUp? '70%':'100%'">
+      <v-card flat :width="$vuetify.display.mdAndUp ? '70%' : '100%'">
         <p class="text-start text-h5 mt-4 ml-4 font-weight-bold"> {{ $t('mua.adminDashboard.mainHeader') }}</p>
 
         <v-card-text class="text-center align-center">
@@ -127,19 +166,16 @@ const options = {
           </v-row>
         </v-card-text>
         <p class="text-start text-h5 my-4 ml-4  font-weight-bold">{{ $t('mua.adminDashboard.chart.headerChart1') }}</p>
-
         <v-card class="text-center w-100 align-center elevation-0 justify-center">
-          <VueApexCharts height="400" type="line" :options="options" :series="series1"></VueApexCharts>
+          <VueApexCharts height="400" type="bar" :options="options" :series="accountsSeries"></VueApexCharts>
         </v-card>
-
         <p class="text-start text-h5 my-10 ml-4  font-weight-bold">{{ $t('mua.adminDashboard.chart.headerChart2') }}</p>
-
         <v-card class="text-center w-100 align-center elevation-0 justify-center">
-          <VueApexCharts height="400" type="line" :options="options" :series="series2"></VueApexCharts>
+          <VueApexCharts height="400" type="bar" :options="options" :series="usersSeries"></VueApexCharts>
         </v-card>
       </v-card>
-      <v-card flat :width="$vuetify.display.mdAndUp? '70%':'100%'">
-      <slot />
+      <v-card flat :width="$vuetify.display.mdAndUp ? '70%' : '100%'">
+        <slot />
       </v-card>
     </v-layout>
   </div>
