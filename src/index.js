e676536ba1a96f88d845a5d98898c0c9fa471a37
AhmedQudaih <ahmedku0123@gmail.com>
2024-12-17 08:16:35 +0200

    mua-plugin

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import MuaErrorMessage from './components/ErrorMessage.vue'
 import { setActivePinia } from 'pinia'
 import 'vuetify/styles';
 import { useAccountsStore, useUsersStore } from "./stores/index.js";
+import AppLoading from './components/AppLoading.vue';
 
 const MuaPlugin = {
   install(app, options = {}) {
     const { router, userComponents = {}, pinia } = options;    
     setActivePinia(pinia);
-    if (router) {
+    /*if (router) {
       useMuaRoutes(router)
       router.beforeEach(async (to, from, next) => {
         await muaBeforeEach(to, from, next)
       });
-    }
+    }*/
     app.component('MuaErrorMessage', MuaErrorMessage)
     if (userComponents.FinalizeRegistrationLinks) {
       app.component('CustomFinalizeRegistrationLinks', userComponents.FinalizeRegistrationLinks);
@@ -37,5 +38,7 @@ export function useMusAccountStore() {
 export function useMusUsersStore() {
   return useUsersStore()
 }
+
+export { AppLoading }
 
 export default MuaPlugin
